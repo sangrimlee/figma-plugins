867ae564c29d5ec4e0e3a931b2f3cd64055fb4f7
sangrimlee <contact@sangrim.dev>
2023-03-23 18:53:04 +0900

    feat: setup plugin
    - setup plugin

---
+++ plugins/generate-style-from-code/src/plugin/index.ts
@@ -0,0 +1,3 @@
+import manifest from 'manifest.json';
+
+figma.showUI(__html__, { title: manifest.name, themeColors: true, width: 640, height: 768 });
---
+++ plugins/generate-style-from-code/src/ui/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div>Generate Style From Code</div>;
+};
+
+export default App;
