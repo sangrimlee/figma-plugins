366b5985f6071b15517df5dd7a422fcfac4b18f3
sangrimlee <contact@sangrim.dev>
2023-03-24 19:57:57 +0900

    feat: add logo icon compnents
    - add logo icon components
    - css, json, tailwindcss, stitches

---
+++ plugins/generate-style-from-code/src/ui/components/icon/CssLogoIcon.tsx
@@ -0,0 +1,12 @@
+const CssLogoIcon = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg {...props} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M5.1 13.5H6.63L6.22125 15.5438L10.5292 17.1592L15.9495 15.3517L16.77 11.25H5.55L5.85 9.75H17.07L17.67 6.75H6.45L6.75 5.25H19.5L17.25 16.5L10.5 18.75L4.5 16.5L5.1 13.5Z"
+        fill="#2197F3"
+      />
+    </svg>
+  );
+};
+
+export default CssLogoIcon;
---
+++ plugins/generate-style-from-code/src/ui/components/icon/index.tsx
@@ -0,0 +1,4 @@
+export { default as CssLogoIcon } from './CssLogoIcon';
+export { default as JsonLogoIcon } from './JsonLogoIcon';
+export { default as StitchesLogoIcon } from './StitchesLogoIcon';
+export { default as TailwindCssLogoIcon } from './TailwindCssLogoIcon';
