56e62fabd69c103789598414aef9c7a9500f4942
sangrimlee <contact@sangrim.dev>
2023-10-31 01:05:48 +0900

    chore: `@figma-plugins/docs` 패키지 추가
    - Storybook 관련 문서를 관리하는 `@figma-plugins/docs` 패키지 추가

---
+++ apps/docs/.storybook/main.ts
@@ -0,0 +1,27 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+import { mergeConfig } from 'vite';
+
+const storybookConfig: StorybookConfig = {
+  framework: '@storybook/react-vite',
+  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.tsx'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-themes',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  staticDirs: ['../public'],
+  typescript: {
+    check: true,
+    reactDocgen: 'react-docgen',
+  },
+  viteFinal(config) {
+    return mergeConfig(config, {
+      build: {
+        chunkSizeWarningLimit: 2048,
+      },
+    });
+  },
+};
+
+export default storybookConfig;
