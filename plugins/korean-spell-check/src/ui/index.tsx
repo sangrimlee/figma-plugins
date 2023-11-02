55dfcf2118c77e5ae18da9d108996aa9a8a20d50
sangrimlee <contact@sangrim.dev>
2023-11-02 18:19:47 +0900

    feat: `Toaster` 추가

--- plugins/korean-spell-check/src/ui/index.tsx
+++ plugins/korean-spell-check/src/ui/index.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Toaster } from '@figma-plugins/ui';
 import { App } from './app';
 
 /* eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- because 'root' element always exists. */
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Toaster />
   </StrictMode>,
 );
