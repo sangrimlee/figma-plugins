7c2cd4d5ffb6b6ef6f461fb058c1182c73a8a8a0
이상림 <contact@sangrim.dev>
2023-11-02 12:19:00 +0900

    feat: `Text` 컴포넌트 추가 (#11)
    - Text 컴포넌트 추가
      - size, weight, variant, align, decoration, truncate, breakWord, lineClamp와 같은 variant를 통한 스타일링
    - Text 컴포넌트에 대한 Story 및 Docs 추가

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,3 +1,4 @@
 export { Box } from './box';
 export { Button, type ButtonVariantProps } from './button';
 export { Flex, type FlexVariantProps } from './flex';
+export { Text, type TextVariantProps } from './text';
