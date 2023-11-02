d7129201c4e9b8f13aa66db7fd3ee59e290ff2a4
이상림 <contact@sangrim.dev>
2023-11-02 16:00:05 +0900

    feat: `Toast` 컴포넌트 추가 (#12)
    - Toast 컴포넌트 추가
    - Toaster 및 useToast를 통한 전역에서 Toast 컴포넌트를 관리
    - Toast 관련 Story 및 Docs 추가

---
+++ packages/ui/src/components/toaster.tsx
@@ -0,0 +1,231 @@
+import { useEffect, useState } from 'react';
+import { Cross2Icon } from '@radix-ui/react-icons';
+import type { ToastProps } from './toast';
+import {
+  Toast,
+  ToastClose,
+  ToastDescription,
+  ToastProvider,
+  ToastTitle,
+  ToastViewport,
+} from './toast';
+import { Box } from './box';
+
+const TOAST_LIMIT = 1;
+const TOAST_REMOVE_DELAY = 50000;
+
+type ToasterToast = ToastProps & {
+  id: string;
+  title?: React.ReactNode;
+  description?: React.ReactNode;
+};
+
+const actionTypes = {
+  ADD_TOAST: 'ADD_TOAST',
+  UPDATE_TOAST: 'UPDATE_TOAST',
+  DISMISS_TOAST: 'DISMISS_TOAST',
+  REMOVE_TOAST: 'REMOVE_TOAST',
+} as const;
+
+let count = 0;
+
+function genId() {
+  count = (count + 1) % Number.MAX_VALUE;
+  return count.toString();
+}
+
+type ActionType = typeof actionTypes;
+
+type Action =
+  | {
+      type: ActionType['ADD_TOAST'];
+      toast: ToasterToast;
+    }
+  | {
+      type: ActionType['UPDATE_TOAST'];
+      toast: Partial<ToasterToast>;
+    }
+  | {
+      type: ActionType['DISMISS_TOAST'];
+      toastId?: ToasterToast['id'];
+    }
+  | {
+      type: ActionType['REMOVE_TOAST'];
+      toastId?: ToasterToast['id'];
+    };
+
+interface ToasterState {
+  toasts: ToasterToast[];
+}
+
+const toastTimeouts = new Map<string, ReturnType<typeof setTimeout>>();
+
+const addToRemoveQueue = (toastId: string) => {
+  if (toastTimeouts.has(toastId)) {
+    return;
+  }
+
+  const timeout = setTimeout(() => {
+    toastTimeouts.delete(toastId);
+    dispatch({
+      type: 'REMOVE_TOAST',
+      toastId,
+    });
+  }, TOAST_REMOVE_DELAY);
+
+  toastTimeouts.set(toastId, timeout);
+};
+
+const reducer = (state: ToasterState, action: Action): ToasterState => {
+  switch (action.type) {
+    case 'ADD_TOAST':
+      return {
+        ...state,
+        toasts: [action.toast, ...state.toasts].slice(0, TOAST_LIMIT),
+      };
+
+    case 'UPDATE_TOAST':
+      return {
+        ...state,
+        toasts: state.toasts.map((t) =>
+          t.id === action.toast.id ? { ...t, ...action.toast } : t,
+        ),
+      };
+
+    case 'DISMISS_TOAST': {
+      const { toastId } = action;
+
+      if (toastId) {
+        addToRemoveQueue(toastId);
+      } else {
+        state.toasts.forEach((t) => {
+          addToRemoveQueue(t.id);
+        });
+      }
+
+      return {
+        ...state,
+        toasts: state.toasts.map((t) =>
+          t.id === toastId || toastId === undefined
+            ? {
+                ...t,
+                open: false,
+              }
+            : t,
+        ),
+      };
+    }
+    case 'REMOVE_TOAST':
+      if (action.toastId === undefined) {
+        return {
+          ...state,
+          toasts: [],
+        };
+      }
+      return {
+        ...state,
+        toasts: state.toasts.filter((t) => t.id !== action.toastId),
+      };
+  }
+};
+
+const listeners: ((state: ToasterState) => void)[] = [];
+
+let memoryState: ToasterState = { toasts: [] };
+
+function dispatch(action: Action) {
+  memoryState = reducer(memoryState, action);
+  listeners.forEach((listener) => {
+    listener(memoryState);
+  });
+}
+
+type Toast = Omit<ToasterToast, 'id'>;
+
+function toast({ ...props }: Toast) {
+  const id = genId();
+
+  const update = (toastProps: ToasterToast) => {
+    dispatch({
+      type: 'UPDATE_TOAST',
+      toast: { ...toastProps, id },
+    });
+  };
+
+  const dismiss = () => {
+    dispatch({ type: 'DISMISS_TOAST', toastId: id });
+  };
+
+  dispatch({
+    type: 'ADD_TOAST',
+    toast: {
+      ...props,
+      id,
+      open: true,
+      onOpenChange: (open) => {
+        if (!open) {
+          dismiss();
+        }
+      },
+    },
+  });
+
+  return {
+    id,
+    dismiss,
+    update,
+  };
+}
+
+function useToast() {
+  const [state, setState] = useState<ToasterState>(memoryState);
+
+  useEffect(() => {
+    listeners.push(setState);
+    return () => {
+      const index = listeners.indexOf(setState);
+      if (index > -1) {
+        listeners.splice(index, 1);
+      }
+    };
+  }, [state]);
+
+  return {
+    ...state,
+    toast,
+    dismiss: (toastId?: string) => {
+      dispatch({ type: 'DISMISS_TOAST', toastId });
+    },
+  };
+}
+
+function Toaster() {
+  const { toasts } = useToast();
+
+  return (
+    <ToastProvider
+      duration={TOAST_REMOVE_DELAY}
+      swipeDirection="up"
+      swipeThreshold={50}
+    >
+      {toasts.map(({ id, title, description, variant, ...props }) => (
+        <Toast key={id} variant={variant} {...props}>
+          <Box css={{ flex: 1 }}>
+            {title ? <ToastTitle>{title}</ToastTitle> : null}
+            {description ? (
+              <ToastDescription variant={variant}>
+                {description}
+              </ToastDescription>
+            ) : null}
+          </Box>
+          <ToastClose variant={variant}>
+            <Cross2Icon height={20} width={20} />
+          </ToastClose>
+        </Toast>
+      ))}
+      <ToastViewport />
+    </ToastProvider>
+  );
+}
+
+export { Toaster, useToast };
--- packages/ui/src/vars/animations.ts
+++ packages/ui/src/vars/animations.ts
@@ -22,6 +22,11 @@ export const animations = {
     },
   }),
 
+  fadeOut: keyframes({
+    '0%': { opacity: 1 },
+    '100%': { opacity: 0 },
+  }),
+
   pulse: keyframes({
     'from, 75%': {
       transform: 'scale(0.85)',
@@ -78,6 +83,36 @@ export const animations = {
     to: {
       transform: 'none',
       opacity: 1,
+    },
+  }),
+
+  /* For Toast */
+  toastSlideIn: keyframes({
+    from: {
+      transform: 'translateY(calc(-100% - 1rem))',
+      opacity: 0,
+    },
+    to: {
+      transform: 'translateX(0)',
+      opacity: 1,
+    },
+  }),
+  toastSlideOut: keyframes({
+    from: {
+      transform: 'translateX(0)',
+      opacity: 1,
+    },
+    to: {
+      transform: 'translateY(calc(-100% - 1rem))',
+      opacity: 0,
+    },
+  }),
+  toastSwipeOut: keyframes({
+    from: {
+      transform: 'translateY(var(--radix-toast-swipe-end-x))',
+    },
+    to: {
+      transform: 'translateY(calc(-100% - 1rem))',
     },
   }),
 };
