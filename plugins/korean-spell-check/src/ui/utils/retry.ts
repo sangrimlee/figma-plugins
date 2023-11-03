7c3706332e127e04e462e168d8d4034c2418d758
sangrimlee <contact@sangrim.dev>
2023-11-03 16:17:10 +0900

    feat: `retry` 관련 유틸 함수 추가
    - Promise를 반환하는 함수를 delay를 가지고 retry하는 유틸 함수 추가

---
+++ plugins/korean-spell-check/src/ui/utils/retry.ts
@@ -0,0 +1,18 @@
+function wait(ms: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+export function retry<Return>(
+  fetchFn: () => Promise<Return>,
+  retries: number,
+  delayMs = 500,
+): Promise<Return> {
+  if (retries === 0) {
+    throw new Error('RETRY_EXCEED');
+  }
+  return fetchFn().catch(() =>
+    wait(delayMs).then(() => retry(fetchFn, retries - 1, delayMs)),
+  );
+}
