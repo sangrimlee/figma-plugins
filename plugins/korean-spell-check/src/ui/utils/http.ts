56b49b2036a02285b1e888cddd487ed3ae0aba21
sangrimlee <contact@sangrim.dev>
2023-11-03 16:09:18 +0900

    feat: http 요청 관련 유틸 함수 추가
    - `fetch`를 통한 http 요청 관련 유틸 함수 추가
    - `params`를 통한 queryParameter 관리 추가
    - `proxy`를 통한 proxy를 거친 요청 추가

---
+++ plugins/korean-spell-check/src/ui/utils/http.ts
@@ -0,0 +1,34 @@
+interface CreateURLOptions {
+  /**
+   * Apply proxy by `https://corsproxy.io/`
+   */
+  proxy?: boolean;
+  /**
+   * `SearchParams` for url
+   */
+  params?: Record<string, string | number>;
+}
+
+function createURL(
+  baseUrl: string,
+  { proxy = false, params = {} }: CreateURLOptions = {},
+) {
+  const url = new URL(baseUrl);
+  Object.entries(params).forEach(([key, value]) => {
+    url.searchParams.set(key, value.toString());
+  });
+  return proxy ? `https://corsproxy.io/?${url.toString()}` : url;
+}
+
+type HttpOptions = RequestInit & CreateURLOptions;
+
+export async function http(
+  url: string,
+  { params, proxy, ...options }: HttpOptions,
+) {
+  const response = await fetch(createURL(url, { params, proxy }), options);
+  if (response.ok) {
+    throw new Error('NETWORK_REQUEST_FAILED');
+  }
+  return response;
+}
