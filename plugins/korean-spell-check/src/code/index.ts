4ced2a5c3560fc3ba83b7942a90a272c39a8018e
sangrimlee <contact@sangrim.dev>
2023-11-12 00:40:18 +0900

    feat: figma의 event handler 추가
    - `selectionchange`, `currentpagechage`,`documentchange` 이벤트 핸들링
    - ui로 부터 오는 message에 대한 핸들링 추가

--- plugins/korean-spell-check/src/code/index.ts
+++ plugins/korean-spell-check/src/code/index.ts
@@ -1,4 +1,6 @@
+import type { UIPluginMessage } from '@/shared/types';
 import manifest from 'manifest.json';
+import { figmaStore } from './figma-store';
 
 figma.skipInvisibleInstanceChildren = true;
 
@@ -8,3 +10,31 @@ figma.showUI(__html__, {
   width: 384,
   height: 448,
 });
+
+figma.on('selectionchange', () => {
+  const { contentType, setTextNodes } = figmaStore.getState();
+  if (contentType === 'layer') {
+    setTextNodes();
+  }
+});
+
+figma.on('currentpagechange', () => {
+  const { contentType, setTextNodes } = figmaStore.getState();
+  if (contentType === 'page') {
+    setTextNodes();
+  }
+});
+
+figma.on('documentchange', () => {
+  figmaStore.getState().setTextNodes();
+});
+
+figma.ui.onmessage = (pluginMessage: UIPluginMessage) => {
+  switch (pluginMessage.type) {
+    case 'ON_CHANGE_CONTENT_TYPE':
+      figmaStore.getState().setContentType(pluginMessage.contentType);
+      break;
+    default:
+      break;
+  }
+};
