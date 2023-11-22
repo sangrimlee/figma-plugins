867ae831eeef3670e63091fc244d4035190fb3dc
이상림 <contact@sangrim.dev>
2023-11-23 01:13:34 +0900

    feat: `Select` 컴포넌트 추가 (#32)
    - `@radix-ui/react-select` 기반 `Select` 컴포넌트 추가
    - `Select` 컴포넌트에 대한 `Story` 및 `Docs` 추가

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -6,5 +6,15 @@ export { Toast, type ToastProps } from './toast';
 export { Toaster, useToast } from './toaster';
 export { Tabs, TabsList, TabsTrigger, TabsContent } from './tabs';
 export { ScrollArea } from './scroll-area';
+export {
+  Select,
+  SelectValue,
+  SelectGroup,
+  SelectTrigger,
+  SelectContent,
+  SelectLabel,
+  SelectItem,
+  SelectSeparator,
+} from './select';
 export { Loader, type LoaderProps } from './loader';
 export { RadioGroup, RadioGroupItem } from './radio-group';
