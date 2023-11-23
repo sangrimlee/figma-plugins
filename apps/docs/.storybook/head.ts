425bfb31d5d7711bdfdea16f16af64d6637c079e
sangrimlee <contact@sangrim.dev>
2023-11-23 21:19:33 +0900

    fix: `manager.css`에 대한 경로 수정
    - Github Pages에서 `manager.css`가 제대로 불러오지 않는 문제를 수정하기 위한 `manager.css`에 대한 `src` 경로 수정

--- apps/docs/.storybook/head.ts
+++ apps/docs/.storybook/head.ts
@@ -9,7 +9,7 @@ export const managerHead = (head: string) => {
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
-    <link href="/styles/manager.css" rel="stylesheet" />
+    <link href="./styles/manager.css" rel="stylesheet" />
   `;
 };
 
