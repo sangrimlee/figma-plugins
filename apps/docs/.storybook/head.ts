47998f15a8e6d7a8ed12ebc369d0c7a94d94420d
sangrimlee <contact@sangrim.dev>
2023-11-23 21:18:36 +0900

    fix: head에 `Inter` 폰트 추가
    - `previewHead` 및 `managerHead`에 `Inter` 폰트를 위한 `link` 태그 추가

--- apps/docs/.storybook/head.ts
+++ apps/docs/.storybook/head.ts
@@ -6,9 +6,18 @@ export const managerHead = (head: string) => {
       property="og:description"
       content="Design System for Figma Plugins."
     />
-    <link
-      rel="stylesheet"
-      href="/styles/manager.css"
-    />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link href="/styles/manager.css" rel="stylesheet" />
+  `;
+};
+
+export const previewHead = (head: string) => {
+  return `
+    ${head}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
   `;
 };
