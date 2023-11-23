5a41ad9e4b966a5c6758c203c9e7401b4c4095ce
sangrimlee <contact@sangrim.dev>
2023-11-24 02:52:16 +0900

    chore: `korean-ipsum` 패키지 추가
    - 한글입숨 플러그인을 위한 `korean-ipsum` 패키지 추가

---
+++ plugins/korean-ipsum/src/code/index.ts
@@ -0,0 +1,8 @@
+import manifest from 'manifest.json';
+
+figma.showUI(__html__, {
+  title: manifest.name,
+  themeColors: true,
+  width: 384,
+  height: 432,
+});
---
+++ plugins/korean-ipsum/src/ui/app.tsx
@@ -0,0 +1,11 @@
+import { globalStyles } from '@figma-plugins/ui';
+
+export function App() {
+  globalStyles();
+
+  return (
+    <div>
+      <h1>한글입숨</h1>
+    </div>
+  );
+}
