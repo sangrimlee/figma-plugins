8180150fdaa22e74f4827b4bc408272343c1dfb3
sangrimlee <contact@sangrim.dev>
2023-11-28 11:37:56 +0900

    feat: `clientStorage` 관련 유틸 함수 추가
    - `figma의 `clientStorage`를 관리하는 유틸 함수 추가

---
+++ plugins/korean-ipsum/src/code/utils/client-storage.ts
@@ -0,0 +1,19 @@
+const STORAGE_KEYS = {
+  FORM_STATE: 'korean-ipsum_form-state',
+} as const;
+
+type StorageKey = keyof typeof STORAGE_KEYS;
+
+export async function getClientStorage<T>(key: StorageKey, defaultValue: T) {
+  const value = (await figma.clientStorage.getAsync(STORAGE_KEYS[key])) as
+    | T
+    | undefined;
+  if (value === undefined) {
+    return defaultValue;
+  }
+  return value;
+}
+
+export function setClientStorage<T>(key: StorageKey, value: T) {
+  return figma.clientStorage.setAsync(STORAGE_KEYS[key], value);
+}
