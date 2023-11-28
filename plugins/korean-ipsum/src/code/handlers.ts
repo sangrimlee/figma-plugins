ada70714a696b0380beedc0ebf31ce85be68488f
sangrimlee <contact@sangrim.dev>
2023-11-28 17:33:19 +0900

    feat: 텍스트 관련 이벤트 핸들링 추가
    - 텍스트를 생성하는 것과 관련된 이벤트 `GENERATE_CONTENT` 및 `GENERATE_CONTENT_FINISHED` 관련 이벤트 핸들링 추가

--- plugins/korean-ipsum/src/code/handlers.ts
+++ plugins/korean-ipsum/src/code/handlers.ts
@@ -1,7 +1,37 @@
 import type { GenerateFormState, UIPluginMessage } from '@/shared/types';
+import { generateContent } from '@/shared/generate';
 import { getClientStorage, setClientStorage } from './utils/client-storage';
 import { postFigmaPluginMessage } from './utils/plugin-message';
 import { isTextNode } from './utils/check-node';
+import { loadFont } from './utils/load-font';
+
+const DELIMITER = {
+  word: ' ',
+  sentence: '\n',
+  paragraph: '\n\n',
+} as const;
+
+async function changeTextNodeCharacters(
+  textNode: TextNode,
+  formState: GenerateFormState,
+) {
+  await loadFont(textNode);
+  const content = generateContent(formState);
+  if (formState.method === 'replace' || textNode.characters.length === 0) {
+    textNode.characters = content;
+  } else {
+    textNode.characters = `${textNode.characters}${
+      DELIMITER[formState.unit]
+    }${content}`;
+  }
+}
+
+function changeSelectionCharacters(formState: GenerateFormState) {
+  const textNodes = figma.currentPage.selection.filter(isTextNode);
+  return Promise.all(
+    textNodes.map((textNode) => changeTextNodeCharacters(textNode, formState)),
+  );
+}
 
 function getIsSelectedTextNode() {
   return figma.currentPage.selection.some(isTextNode);
@@ -33,7 +63,14 @@ export async function messageEventHandler(msg: UIPluginMessage) {
       });
       return;
     case 'ON_CHANGE_FORM_STATE':
-      return setClientStorage('FORM_STATE', msg.formState);
+      await setClientStorage('FORM_STATE', msg.formState);
+      break;
+    case 'GENERATE_CONTENT':
+      await changeSelectionCharacters(msg.formState);
+      postFigmaPluginMessage({
+        type: 'GENERATE_CONTENT_FINISHED',
+      });
+      break;
     default:
       break;
   }
