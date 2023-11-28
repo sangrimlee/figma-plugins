63e686f7982dcef032ca7ad3983d2591ad721cf3
sangrimlee <contact@sangrim.dev>
2023-11-28 14:30:24 +0900

    feat: `onmessage` 이벤트 핸들링 추가
    - `figma`의 `onmessage` 이벤트 핸들링 추가

--- plugins/korean-ipsum/src/code/index.ts
+++ plugins/korean-ipsum/src/code/index.ts
@@ -1,6 +1,8 @@
 import manifest from 'manifest.json';
+import type { GenerateFormState, UIPluginMessage } from '@/shared/types';
 import { postFigmaPluginMessage } from './utils/plugin-message';
 import { isTextNode } from './utils/check-node';
+import { getClientStorage, setClientStorage } from './utils/client-storage';
 
 figma.showUI(__html__, {
   title: manifest.name,
@@ -15,3 +17,30 @@ figma.on('selectionchange', () => {
     isSelectedTextNode: figma.currentPage.selection.some(isTextNode),
   });
 });
+
+async function messageEventHandler(msg: UIPluginMessage) {
+  switch (msg.type) {
+    case 'INIT':
+      postFigmaPluginMessage({
+        type: 'ON_INIT',
+        isSelectedTextNode: figma.currentPage.selection.some(isTextNode),
+        formState: await getClientStorage<GenerateFormState>('FORM_STATE', {
+          source: 'countingStars',
+          unit: 'word',
+          count: '1',
+          method: 'replace',
+        }),
+      });
+      break;
+    case 'ON_CHANGE_FORM_STATE':
+      await setClientStorage('FORM_STATE', msg.formState);
+      break;
+    default:
+      break;
+  }
+}
+
+figma.ui.onmessage = (msg: UIPluginMessage) => {
+  // eslint-disable-next-line @typescript-eslint/no-floating-promises -- TODO: Add handling Errors
+  messageEventHandler(msg);
+};
