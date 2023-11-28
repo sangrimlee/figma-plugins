0839b93e1971707ab064be80a4b9ea7e04ecd0ff
sangrimlee <contact@sangrim.dev>
2023-11-28 14:36:41 +0900

    feat: 로딩 중 화면 추가
    - 로딩 중을 표시하는 화면 추가

--- plugins/korean-ipsum/src/ui/app.tsx
+++ plugins/korean-ipsum/src/ui/app.tsx
@@ -1,15 +1,29 @@
-import { globalStyles } from '@figma-plugins/ui';
+import { Flex, Loader, colors, globalStyles } from '@figma-plugins/ui';
 import { FormPage, MainPage } from './pages';
 import { useMessageEventListener } from './hooks';
 import { useGlobalStore } from './store';
 
 export function App() {
+  const { isLoading } = useMessageEventListener();
   const isSelectedTextNode = useGlobalStore(
     (state) => state.isSelectedTextNode,
   );
-
   globalStyles();
-  useMessageEventListener();
+
+  if (isLoading) {
+    return (
+      <Flex css={{ height: '100vh' }} items="center" justify="center">
+        <Loader
+          animate
+          css={{
+            color: colors.icon.secondary.default,
+          }}
+          height="24"
+          width="24"
+        />
+      </Flex>
+    );
+  }
 
   return <>{isSelectedTextNode ? <FormPage /> : <MainPage />}</>;
 }
