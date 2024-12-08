bc274ab812bb209a35966491d52d8c5311383941
sangrimlee <contact@sangrim.dev>
2024-12-08 21:03:58 +0900

    feat: `prettier` 설정 수정
    - `.hbs` 파일에 대한 `prettier`의 `parser`을 다르게 적용하도록 변경

--- packages/config/prettier/index.js
+++ packages/config/prettier/index.js
@@ -10,6 +10,39 @@ const config = {
   singleQuote: true,
   endOfLine: 'lf',
   plugins: ['prettier-plugin-packagejson'],
+
+  overrides: [
+    {
+      files: '*.html.hbs',
+      options: {
+        parser: 'html',
+      },
+    },
+    {
+      files: '*.json.hbs',
+      options: {
+        parser: 'json',
+      },
+    },
+    {
+      files: '*.json.hbs',
+      options: {
+        parser: 'json',
+      },
+    },
+    {
+      files: '*.{js,cjs,mjs}.hbs',
+      options: {
+        parser: 'babel',
+      },
+    },
+    {
+      files: '*.{ts,tsx}.hbs',
+      options: {
+        parser: 'typescript',
+      },
+    },
+  ],
 };
 
 // eslint-disable-next-line import/no-default-export -- 'configuration file should be use default export'
